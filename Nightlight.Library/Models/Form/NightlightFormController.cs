a5d72be9e47f4b2b44ecac9fcfb0c505d68f2458
Michael Campbell <mikercampbell@outlook.com>
2017-11-15 21:04:13 -0700

    Added FormController

---
+++ Nightlight.Library/Models/Form/NightlightFormController.cs
@@ -0,0 +1,31 @@
+﻿using Nightlight.Models.Nodes;
+using Nightlight.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nightlight.Models.Form
+{
+    public class NightlightFormController<T>
+    {
+        public IEnumerable<INightlightNode> Nodes { get; set; }
+        
+        public bool AllFieldsValid { get; set; }
+
+        public NightlightFormController(T obj)
+        {
+            var attributes = AttributeUtils.ReadAttributes(obj);
+
+            Nodes = AttributeUtils.GetNodesFromAttributes(attributes);
+
+            // validate all the nodes.  I did it here arbitrarily, thinking
+            // that validation at build, rather on the node creation,
+            // would hide the contents of the library
+            foreach(var node in Nodes)
+            {
+                node.ValidateNode();
+            }
+        }
+    }
+}
